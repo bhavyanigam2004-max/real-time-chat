d7e018ba29c2a9c47ccc82d28b2c1942c6db9af4
Bhavya Nigam <bhavyanigam2004@gmail.com>
2026-04-19 16:46:14 +0530

    Change SameSite cookie attribute to 'None'

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -25,7 +25,7 @@ const token=await genToken(user._id)
 res.cookie("token",token,{
     httpOnly:true,
     maxAge:7*24*60*60*1000,
-    sameSite:"Strict",
+    sameSite:"None",
     secure:true
 })
 return res.status(201).json({
@@ -59,7 +59,7 @@ const token=await genToken(user._id)
 res.cookie("token",token,{
     httpOnly:true,
     maxAge:7*24*60*60*1000,
-    sameSite:"Strict",
+    sameSite:"None",
     secure:true
 })
 return res.status(201).json({
